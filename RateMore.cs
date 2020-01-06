0c28fe691911eb7d0b4c1145d867bb5af05a5293
hamza herbou <35342314+herbou@users.noreply.github.com>
2020-01-06 17:52:43 +0100

    Update

--- RateMore.cs
+++ RateMore.cs
@@ -5,11 +5,13 @@ public class RateMore : MonoBehaviour
 
 	public void Rate ()
 	{
+		// "market" works for android  (iOS: put your app link
 		Application.OpenURL ("market://details?id=com.test.testtest");
 	}
 
 	public void More ()
 	{
+		// Android  ,(iOS: put you app store link)
 		Application.OpenURL ("https://play.google.com/store/apps/dev?id=1551996653619230352");
 	}
 
