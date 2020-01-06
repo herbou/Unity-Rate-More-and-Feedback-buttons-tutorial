cdd25578a15c630b8a3a9e118b5108b3255b0537
hamza herbou <35342314+herbou@users.noreply.github.com>
2020-01-06 17:11:48 +0100

    Add files via upload

---
+++ RateMore.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class RateMore : MonoBehaviour
+{
+
+	public void Rate ()
+	{
+		Application.OpenURL ("market://details?id=com.test.testtest");
+	}
+
+	public void More ()
+	{
+		Application.OpenURL ("https://play.google.com/store/apps/dev?id=1551996653619230352");
+	}
+
+	public void Feedback ()
+	{
+		Application.OpenURL ("mailto:youremail@gmail.com");
+	}
+}
